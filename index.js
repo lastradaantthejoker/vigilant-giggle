586388c6574c79525d8daf6ba7df28df550eed77
lastradaantthejoker <130905789+lastradaantthejoker@users.noreply.github.com>
2023-04-16 13:56:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = require("axios");
+var url = [];
+var urlz = process.env.THEURL;
+axios_1.default.get(urlz).then(function (e) {
+    e.data.toString().split("\n").forEach(function (e) {
+        if (e.startsWith("https://")) {
+            url.push(e);
+        }
+    });
+});
+setTimeout(function () {
+    url.forEach(function (e) {
+        axios_1.default.get(e).then(function (r) {
+            console.log("dun");
+        }).catch(function () { });
+    });
+}, 2500);
